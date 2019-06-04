244fefc04531f139d1cae7fa11f15ff9785fba5e
Flynn <wphestiraid@gmail.com>
2019-06-04 22:33:35 +0900

    ormConfig 추가

---
+++ src/ormConfig.ts
@@ -0,0 +1,20 @@
+import { ConnectionOptions } from 'typeorm';
+
+const connectionOptions: ConnectionOptions = {
+  type: 'postgres',
+  database: 'boilerplate',
+  port: 5432,
+  host: process.env.DB_ENDPOINT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  synchronize: true,
+  logging: true,
+  entities: ['entity/**/*.ts'],
+  migrations: ['migration/**/*.ts'],
+  cli: {
+    entitiesDir: 'entity',
+    migrationsDir: 'migration'
+  }
+};
+
+export default connectionOptions;
