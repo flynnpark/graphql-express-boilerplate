ab384f9f3d56875da7cc5ee9975a6844ef226019
Flynn <wphestiraid@gmail.com>
2019-06-04 22:25:44 +0900

    비밀번호 해시 및 비교 함수 추가
    - tslint-config-prettier 추가

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,9 +1,14 @@
+import { compare, hash } from 'bcrypt';
 import {
+  BeforeInsert,
+  BeforeUpdate,
   Column,
   CreateDateColumn,
   Entity,
   PrimaryGeneratedColumn
 } from 'typeorm';
+
+const BCRYPT_ROUNDS = 100;
 
 @Entity()
 class User {
@@ -33,6 +38,23 @@ class User {
 
   @Column({ type: 'varchar' })
   facebookId: string | null;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  savePassword = async (): Promise<void> => {
+    if (this.password) {
+      const hashedPassword = await this.hashPassword(this.password);
+      this.password = hashedPassword;
+    }
+  };
+
+  hashPassword = async (password: string): Promise<string> => {
+    return hash(password, BCRYPT_ROUNDS);
+  };
+
+  comparePassword = async (password: string): Promise<boolean> => {
+    return compare(password, this.password);
+  };
 }
 
 export default User;
