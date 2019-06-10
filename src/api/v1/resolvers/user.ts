28c8d44535d0cc7f9c6c120d38685cd8cce107a2
Flynn <wphestiraid@gmail.com>
2019-06-10 23:18:12 +0900

    로그인 구현
    - JWT 처리는 나중에......

--- src/api/v1/resolvers/user.ts
+++ src/api/v1/resolvers/user.ts
@@ -38,6 +38,45 @@ const resolvers = {
           token: null
         };
       }
+    },
+    emailSignIn: async (
+      _: null | undefined,
+      { email, password }
+    ): Promise<{
+      ok: boolean;
+      error: string | null;
+      token: string | null;
+    }> => {
+      try {
+        const user = await User.findOne({ email });
+        if (!user) {
+          return {
+            ok: false,
+            error: 'No user found with that email',
+            token: null
+          };
+        }
+        const checkPassword = await user.comparePassword(password);
+        if (checkPassword) {
+          return {
+            ok: true,
+            error: null,
+            token: 'TOKEN WILL BE HERE'
+          };
+        } else {
+          return {
+            ok: false,
+            error: 'Wrong password',
+            token: null
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error,
+          token: null
+        };
+      }
     }
   }
 };
